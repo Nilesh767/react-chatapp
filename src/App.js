4bf9c32d493df96c070f706947d775c81bc327af
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2021-01-30 18:45:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,141 @@
+import "./App.css";
+
+import React, { useRef, useState } from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDPyOhf2rerbvHTOfNeghm38jumUadTdIY",
+  authDomain: "chatapp-cacbb.firebaseapp.com",
+  projectId: "chatapp-cacbb",
+  storageBucket: "chatapp-cacbb.appspot.com",
+  messagingSenderId: "719078959209",
+  appId: "1:719078959209:web:6d8a9e23c4e68f77c61af8",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <header>
+        <h1>💬</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign In with Google
+      </button>
+      <p1>Welcome to Neo's Chat App. I talk to myself here!</p1>
+    </>
+  );
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+
+  const [messages] = useCollectionData(query, { idField: "id" });
+
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue("");
+
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button
+          type="submit"
+          disabled={
+            !formValue ||
+            formValue.replace(/^\s+/, "").replace(/\s+$/, "") === ""
+          }
+        >
+          🕊️
+        </button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL, createdAt } = props.message;
+  var n = new Date(createdAt * 1000).toLocaleTimeString();
+
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL ||
+            "https://i0.wp.com/bane-tech.com/wp-content/uploads/2015/10/N.png?ssl=1"
+          }
+          alt=""
+        />
+        <p1>
+          {text}
+          <p>{n}</p>
+        </p1>
+      </div>
+    </>
+  );
+}
+
+export default App;
